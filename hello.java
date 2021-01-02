0386cf10b927661af3a2c28e784cb8423bc32d27
Monwar Hossain <monwarhossainmunna@gmail.com>
2021-01-02 23:10:46 +0600

    Added a new method called display to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,11 @@
 public class hello{
     public static void main(String[] args){
-        System.out.println("vs code is awesome...")
+        System.out.println("vs code is awesome...");
+
+        getting("Monwar Hossain")
+    }
+
+    public static void getting(String name){
+        System.out.println("welcome "+name);
     }
 }
